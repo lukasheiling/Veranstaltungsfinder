2066370f141c553274ee5fa83845544a3d32b3cd
lukasheiling <55692511+lukasheiling@users.noreply.github.com>
2024-04-05 11:23:41 +0200

    neues_vue

--- veranstaltungsfinder/src/App.vue
+++ veranstaltungsfinder/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Events</h1>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }} - {{ event.date }} - {{ event.venue }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      events: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/events/');
+      this.events = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Deine Styles hier */
 </style>
